4f9dc4e5559a22f3f44c8e0ca8d83fa9ba43187a
Martyn Bissett <martynbissett@yahoo.co.uk>
2016-07-22 15:56:17 +0900

    Start core module with module class

--- public/index.php
+++ public/index.php
@@ -23,67 +23,79 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 
-// ==================================
+
+
 // Instantiate the app
+$settings = require APPLICATION_PATH . '/src/settings.php';
+$app = new Slim\App($settings);
 
-// Compile app settings
-// $settings = require APPLICATION_PATH . '/src/settings.php';
-$settings = [];
-if ($path = realpath(APPLICATION_PATH . '/src/settings.' . APPLICATION_ENV . '.php')) {
-    $settings = array_merge_recursive($settings, require $path);
-}
-
-// Compile module settings
-$modulePaths = array_filter(glob(APPLICATION_PATH . '/src/*'), 'is_dir');
-foreach ($modulePaths as $modulePath) {
-    if ($path = realpath($modulePath . '/settings.php')) {
-        $settings = array_merge_recursive($settings, require $path);
-    }
-}
-
-$app = new \Slim\App($settings);
-
-
-// ==================================
-// Set up dependencies
-
-// // Load app dependencies
-// require APPLICATION_PATH . '/src/dependencies.php';
-
-// Load module dependencies
-foreach ($modulePaths as $modulePath) {
-    if ($path = realpath($modulePath . '/dependencies.php')) {
-        require $path;
-    }
-}
-
-// ==================================
-// Register middleware
-
-// // Load app middleware
-// require APPLICATION_PATH . '/src/middleware.php';
-
-// Load module middleware
-foreach ($modulePaths as $modulePath) {
-    if ($path = realpath($modulePath . '/middleware.php')) {
-        require $path;
-    }
-}
-
-// ==================================
-// Register routes
-
-// // Load app routes
-// require APPLICATION_PATH . '/src/routes.php';
-
-// Load module routes
-foreach ($modulePaths as $modulePath) {
-    if ($path = realpath($modulePath . '/routes.php')) {
-        require $path;
-    }
-}
-
-
-// ==================================
-// Run app
+// initialize all modules in settings > modules > autoload [...]
+$moduleInitializer = new MartynBiz\Slim\Module\Initializer($settings['settings']['module_initializer']);
+$moduleInitializer->initModules($app);
 $app->run();
+
+
+// // ==================================
+// // Instantiate the app
+//
+// // Compile app settings
+// // $settings = require APPLICATION_PATH . '/src/settings.php';
+// $settings = [];
+// if ($path = realpath(APPLICATION_PATH . '/src/settings.' . APPLICATION_ENV . '.php')) {
+//     $settings = array_merge_recursive($settings, require $path);
+// }
+//
+// // Compile module settings
+// $modulePaths = array_filter(glob(APPLICATION_PATH . '/src/*'), 'is_dir');
+// foreach ($modulePaths as $modulePath) {
+//     if ($path = realpath($modulePath . '/settings.php')) {
+//         $settings = array_merge_recursive($settings, require $path);
+//     }
+// }
+//
+// $app = new \Slim\App($settings);
+//
+//
+// // ==================================
+// // Set up dependencies
+//
+// // // Load app dependencies
+// // require APPLICATION_PATH . '/src/dependencies.php';
+//
+// // Load module dependencies
+// foreach ($modulePaths as $modulePath) {
+//     if ($path = realpath($modulePath . '/dependencies.php')) {
+//         require $path;
+//     }
+// }
+//
+// // ==================================
+// // Register middleware
+//
+// // // Load app middleware
+// // require APPLICATION_PATH . '/src/middleware.php';
+//
+// // Load module middleware
+// foreach ($modulePaths as $modulePath) {
+//     if ($path = realpath($modulePath . '/middleware.php')) {
+//         require $path;
+//     }
+// }
+//
+// // ==================================
+// // Register routes
+//
+// // // Load app routes
+// // require APPLICATION_PATH . '/src/routes.php';
+//
+// // Load module routes
+// foreach ($modulePaths as $modulePath) {
+//     if ($path = realpath($modulePath . '/routes.php')) {
+//         require $path;
+//     }
+// }
+//
+//
+// // ==================================
+// // Run app
+// $app->run();
---
+++ src/settings.php
@@ -0,0 +1,30 @@
+<?php
+
+// App settings
+$appSettings = [
+    'settings' => [
+        'module_dir' => APPLICATION_PATH . '/src/',
+        'module_initializer' => [
+            'modules' => [
+                'martynbiz-core' => 'MartynBiz\\Slim\\Modules\\Core\\Module',
+                // 'martynbiz-auth' => 'MartynBiz\\Slim\\Modules\\Auth\\Module',
+                // 'martynbiz-blog' => 'MartynBiz\\Slim\\Modules\\Blog\\Module',
+            ],
+        ],
+    ],
+];
+
+// Module Settings
+$moduleSettings = [];
+foreach ($appSettings['settings']['module_initializer'] as $moduleName => $moduleClassName) {
+    if ($path = realpath($appSettings['settings']['module_dir'] . $moduleName . '/settings.php')) {
+        $moduleSettings = array_merge_recursive($moduleSettings, require $path);
+    }
+}
+
+// Environment settings
+if ($path = realpath(APPLICATION_PATH . '/src/settings.' . APPLICATION_ENV . '.php')) {
+    $envSettings = require $path;
+}
+
+return array_merge_recursive($moduleSettings, $appSettings, $envSettings);
